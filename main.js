65015665718d36aac9a52da84320289e902bd957
Dana-Sarkawt <91015045+Dana-Sarkawt@users.noreply.github.com>
2023-03-05 23:59:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+$(".sub-cont").on("click", function(){
+    $(".img").toggleClass("down")
+})
+
+$(".sub-cont").on("click", function(){
+    $(".cont-list").toggleClass("non-active")
+})
+
+
+
+
+$(".sub-cont-1").on("click", function(){
+    $(".img-1").toggleClass("down-1")
+})
+
+$(".sub-cont-1").on("click", function(){
+    $(".cont-list-1").toggleClass("non-active")
+})
+
+
+
+
+$(".sub-cont-2").on("click", function(){
+    $(".img-2").toggleClass("down-2")
+})
+
+$(".sub-cont-2").on("click", function(){
+    $(".cont-list-2").toggleClass("non-active")
+})
+  
